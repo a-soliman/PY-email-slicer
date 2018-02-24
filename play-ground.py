f8c5aee2d25bdbace8bc84b308c4c4210ad322e8
a-soliman <ahmed.soliman@programmer.net>
2018-02-24 09:17:47 -0800

    using shortcuts

--- play-ground.py
+++ play-ground.py
@@ -8,3 +8,15 @@ print(word[2])
 # slicing
 print(space)
 print(word[0:6:1])
+
+print(space)
+
+awesome = word[13:20:1]
+print(awesome)
+
+# SHORTCUTS
+print(word[7:])
+print(space)
+print(word[7::2])
+print(space)
+print(word[:6])
