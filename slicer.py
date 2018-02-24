667135ff93fa3ee409c8db830edacc7bf3ea4fab
a-soliman <ahmed.soliman@programmer.net>
2018-02-24 09:35:10 -0800

    collecting email address

--- slicer.py
+++ slicer.py
@@ -1,7 +1,9 @@
 # GET USER'S EMAIL ADDRESS
+email_address = input('PLease enter your email address : ')
+email_address = email_address.lower()
 
+# SLICE OUT THE USER NAME
 
-# SLICE OUT THE USER NAE
 
 # SLICE THE DOMAIN NAME
 
